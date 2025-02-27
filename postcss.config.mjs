022476ca16b1b9a509dfb96209bf1960bae64959
william <william@carrot-i.com>
2025-02-27 13:33:27 +0900

    #수정사항 - 빌드 에러 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
-    autoprefixer: {},
-  }
+  },
 };
 
 export default config;
