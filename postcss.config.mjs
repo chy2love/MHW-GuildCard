f198f5a4aac6703dcd607e407bed74704bef4ae3
william <william@carrot-i.com>
2025-02-27 13:41:05 +0900

    #수정사항 - 빌드 에러 수정

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [
-    'postcss-nesting', // TailwindCSS보다 먼저 추가해야 함
-    'tailwindcss',
-    'autoprefixer',
-  ],
+  plugins: {
+    'postcss-nesting':{}, // TailwindCSS보다 먼저 추가해야 함
+    'tailwindcss':{},
+    'autoprefixer':{},
+  },
 };
 
 export default config;
